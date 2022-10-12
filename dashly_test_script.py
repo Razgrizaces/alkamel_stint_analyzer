7ee6a8021eca9e06bc316a83b42fab2bef073703
Tristen <tristen.terracciano@gmail.com>
2022-10-12 18:08:12 -0400

    Added more stuff to the dashly file.

--- dashly_test_script.py
+++ dashly_test_script.py
@@ -40,7 +40,7 @@ app.layout = html.Div(
             className="header",
         ),
         html.Div(
-            #pickers, but I think this is weird/wrong, bc it doesn't do what I want, let's try to use dbc
+            #pickers
             children=[
                     html.Div(
                         children=[
@@ -126,11 +126,6 @@ app.layout = html.Div(
                     ),
                     className="card",
                 ),
-            ],
-            className="wrapper",
-        ),
-        html.Div(
-            children=[
                 html.Div(
                     children=dcc.Graph(
                         id="position_plot", 
@@ -141,13 +136,84 @@ app.layout = html.Div(
             ],
             className="wrapper",
         ),
-        
+        html.Div(
+            #pickers
+            children=[
+                    html.Div(
+                        children=[
+                            #team picker
+                            html.Div(children="Team", className="menu-title"),
+                            dcc.Dropdown(
+                                id="team_filter",
+                                options=[
+                                ],
+                                value="FIA WEC",
+                                clearable=False,
+                                className="dropdown",
+                            )
+                        ],className = 'menu-item'
+                    ),
+                    html.Div(
+                        children=[
+                            #driver picker
+                            html.Div(children="Driver", className="menu-title"),
+                            #these use dcc dropdowns, so we'll have to swap to dbc
+                            dcc.Dropdown(
+                                id="driver_filter",
+                                options=[
+                                ],
+                                clearable=False,
+                                className="dropdown",
+                            )
+                        ],className = 'menu-item'
+                    ),
+                ],
+                className='second-menu',
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="driver_lap_time_plot", 
+                        config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
+        ),
     ]
 )
 
-#callbacks/functions for the filters
 
-#
+
+#callbacks/functions for the filters
+@app.callback(
+    Output('team_filter', 'options'),
+    Input('championship_filter', 'value'),
+    Input('season_filter', 'value'),
+    Input('circuit_filter', 'value'),
+    Input('wec_class_filter', 'value'),
+    Input('session_filter', 'value')
+)
+def set_team_options(championship, season, circuit, wec_class, session):
+    fia_wec_data_filtered = filter_others(fia_wec_data, championship, season, circuit, wec_class, session, "", "")
+    return [{'label': i, 'value': i} for i in fia_wec_data_filtered.team_no.unique()]
+
+@app.callback(
+    Output('driver_filter', 'options'),
+    Input('championship_filter', 'value'),
+    Input('season_filter', 'value'),
+    Input('circuit_filter', 'value'),
+    Input('wec_class_filter', 'value'),
+    Input('session_filter', 'value'),
+    Input('team_filter', 'value')
+)
+def set_driver_options(championship, season, circuit, wec_class, session, team_no):
+    #selector for championship
+    fia_wec_data_filtered = filter_others(fia_wec_data, championship, season, circuit, wec_class, session, team_no, "")
+    return [{'label': i, 'value': i} for i in fia_wec_data_filtered.driver_name.unique()]
+
 @app.callback(
     Output('circuit_filter', 'options'),
     Input('season_filter', 'value')
@@ -162,6 +228,86 @@ def set_circuit_options(selected):
 def set_season_options(selected):
     return[{'label': i, 'value': i} for i in fia_wec_data[fia_wec_data['championship']==selected].season.unique()]
 
+def filter_others(fia_wec_data, championship, season, circuit, wec_class, session, team_no, driver_name):
+    #selector for championship
+    fia_wec_data_filtered = fia_wec_data[fia_wec_data['championship'] == championship]
+    #selector for year/season
+    fia_wec_data_filtered = fia_wec_data_filtered[fia_wec_data_filtered['season'] == season]
+    #selector for circuit
+    fia_wec_data_filtered = fia_wec_data_filtered[fia_wec_data_filtered['circuit_season'] == circuit]
+    #selector for session
+    fia_wec_data_filtered = fia_wec_data_filtered[fia_wec_data_filtered['session'] == session]
+    #filter selector for class
+    fia_wec_data_filtered = filter_class(fia_wec_data_filtered, wec_class)
+    #potentially unused filter for team_no
+    if(team_no != ""):
+        fia_wec_data_filtered = fia_wec_data_filtered[fia_wec_data_filtered['team_no'] == team_no]
+    if(driver_name != ""):
+        fia_wec_data_filtered = fia_wec_data_filtered[fia_wec_data_filtered['driver_name'] == driver_name]
+    #fix index
+    fia_wec_data_filtered = fia_wec_data_filtered.reset_index(drop=True)
+    return fia_wec_data_filtered
+
+def filter_class(fia_wec_data, wec_class):
+    if(wec_class != 'Overall'):
+        if wec_class == 'LMGTE':
+            fia_wec_data = fia_wec_data[(fia_wec_data['class'] == 'LMGTE Pro')|(fia_wec_data['class'] == 'LMGTE Am')]
+        elif wec_class == 'LMPs':
+            fia_wec_data = fia_wec_data[(fia_wec_data['class'] == 'LMP2')|(fia_wec_data['class'] == 'LMP1')|\
+                (fia_wec_data['class'] == 'LMP1-H')|(fia_wec_data['class'] == 'LMP1-L')|\
+                    (fia_wec_data['class'] == 'HYPERCAR')]
+        elif wec_class == 'CNDT (LeMans Only)':
+            fia_wec_data = fia_wec_data[(fia_wec_data['class'] == 'CDNT')| (fia_wec_data['class'] == 'INNOVATIVE CAR')]
+        elif wec_class == 'LMP1-H (2016 Only)':
+            fia_wec_data = fia_wec_data[(fia_wec_data['class'] == 'LMP1-H')]
+        elif wec_class == 'LMP1-L (2016 Only)':
+            fia_wec_data = fia_wec_data[(fia_wec_data['class'] == 'LMP1-L')]
+        elif wec_class == 'LMP1/Hypercar':
+            fia_wec_data = fia_wec_data[(fia_wec_data['class'] == 'LMP1')|(fia_wec_data['class'] == 'HYPERCAR')|\
+                (fia_wec_data['class'] == 'LMP1-H')|(fia_wec_data['class'] == 'LMP1-L')]
+        else:
+            fia_wec_data = fia_wec_data[(fia_wec_data['class'] == wec_class)]
+    return fia_wec_data
+
+#callback for the first charts
+@app.callback(
+    [Output("driver_lap_time_plot", "figure")],
+    [
+        Input("championship_filter", "value"),
+        Input("season_filter", "value"),
+        Input("circuit_filter", "value"),
+        Input("wec_class_filter", "value"),
+        Input("session_filter", "value"),
+        Input('team_filter', 'value'),
+        Input('driver_filter', 'value')
+    ],
+)
+def update_dlt_plot(championship, season, circuit, wec_class, session, team_no, driver_name):
+    fia_wec_data_filtered = filter_others(fia_wec_data, championship, season, circuit, wec_class, session, team_no, driver_name)
+    fia_wec_data_filtered = filter_class(fia_wec_data_filtered, wec_class)
+    fia_wec_data_filtered = fia_wec_data_filtered.reset_index(drop=True)
+
+    #this is the position plot format.
+    color_sequence = px.colors.qualitative.Alphabet
+    #adding a lap time plot as well
+    cutoff_time = 0
+    if(wec_class == 'Overall'):
+        #factor for cutoff time is changed a bit to show all laps. I think 1.6 works? 
+        cutoff_time = fia_wec_data_filtered['lap_time_ms'].min()*1.6
+        box_plot_title = '160% Time Box Plots'
+    elif (wec_class == 'LMPs'):
+    #ah if it's lmps, should be a bit smaller
+        cutoff_time = fia_wec_data_filtered['lap_time_ms'].min()*1.3
+        box_plot_title = '130% Time Box Plots'
+    else:
+        cutoff_time = fia_wec_data_filtered['lap_time_ms'].min()*1.1
+        box_plot_title = '110% Time Box Plots'
+    fia_wec_data_with_cutoff_time = fia_wec_data_filtered[fia_wec_data_filtered['lap_time_ms'] < cutoff_time]
+    driver_lap_time_plot = px.box(fia_wec_data_with_cutoff_time, y = 'team_no', x = 'lap_time_s', color = "team_no", color_discrete_sequence=color_sequence,title=box_plot_title)
+    driver_lap_time_plot.update_layout(paper_bgcolor="black",plot_bgcolor="black", legend_font_color="white")
+    return driver_lap_time_plot
+
+#callback for the second charts
 @app.callback(
     [Output("position_plot", "figure"), Output("lap_time_plot", "figure")],
     [
@@ -172,41 +318,17 @@ def set_season_options(selected):
         Input("session_filter", "value"),
     ],
 )
-def update_charts(championship, season, circuit, wec_class, session):
-
-    #selector for championship
-    fia_wec_data_filtered = fia_wec_data[fia_wec_data['championship'] == championship]
-    #selector for round
-    fia_wec_data_filtered = fia_wec_data_filtered[fia_wec_data_filtered['season'] == season]
-    fia_wec_data_filtered = fia_wec_data_filtered[fia_wec_data_filtered['circuit_season'] == circuit]
-    #selector for session
-    fia_wec_data_filtered = fia_wec_data_filtered[fia_wec_data_filtered['session'] == session]
-    #selector for class
-    if(wec_class != 'Overall'):
-        if wec_class == 'LMGTE':
-            fia_wec_data_filtered = fia_wec_data_filtered[(fia_wec_data_filtered['class'] == 'LMGTE Pro')|(fia_wec_data_filtered['class'] == 'LMGTE Am')]
-        elif wec_class == 'LMPs':
-            fia_wec_data_filtered = fia_wec_data_filtered[(fia_wec_data_filtered['class'] == 'LMP2')|(fia_wec_data_filtered['class'] == 'LMP1')|\
-                (fia_wec_data_filtered['class'] == 'LMP1-H')|(fia_wec_data_filtered['class'] == 'LMP1-L')|\
-                    (fia_wec_data_filtered['class'] == 'HYPERCAR')]
-        elif wec_class == 'CNDT (LeMans Only)':
-            fia_wec_data_filtered = fia_wec_data_filtered[(fia_wec_data_filtered['class'] == 'CDNT')| (fia_wec_data_filtered['class'] == 'INNOVATIVE CAR')]
-        elif wec_class == 'LMP1-H (2016 Only)':
-            fia_wec_data_filtered = fia_wec_data_filtered[(fia_wec_data_filtered['class'] == 'LMP1-H')]
-        elif wec_class == 'LMP1-L (2016 Only)':
-            fia_wec_data_filtered = fia_wec_data_filtered[(fia_wec_data_filtered['class'] == 'LMP1-L')]
-        elif wec_class == 'LMP1/Hypercar':
-            fia_wec_data_filtered = fia_wec_data_filtered[(fia_wec_data_filtered['class'] == 'LMP1')|(fia_wec_data_filtered['class'] == 'HYPERCAR')|\
-                (fia_wec_data_filtered['class'] == 'LMP1-H')|(fia_wec_data_filtered['class'] == 'LMP1-L')]
-        else:
-            fia_wec_data_filtered = fia_wec_data_filtered[(fia_wec_data_filtered['class'] == wec_class)]
+def update_other_plots(championship, season, circuit, wec_class, session):
+    fia_wec_data_filtered = filter_others(fia_wec_data, championship, season, circuit, wec_class, session, "", "")
+    fia_wec_data_filtered = filter_class(fia_wec_data_filtered, wec_class)
     fia_wec_data_filtered = fia_wec_data_filtered.reset_index(drop=True)
 
     #this is the position plot format.
+    color_sequence = px.colors.qualitative.Alphabet
     if(wec_class == 'Overall' or wec_class == 'LMGTE' or wec_class == 'LMPs'):
-        position_plot = px.line(fia_wec_data_filtered, x = 'elapsed_s', y = 'position', color = "team_no", color_discrete_sequence=px.colors.qualitative.Alphabet)
+        position_plot = px.line(fia_wec_data_filtered, x = 'elapsed_s', y = 'position', color = "team_no", color_discrete_sequence=color_sequence)
     else:
-        position_plot = px.line(fia_wec_data_filtered, x = 'elapsed_s', y = 'class_position', color = "team_no", color_discrete_sequence=px.colors.qualitative.Alphabet)
+        position_plot = px.line(fia_wec_data_filtered, x = 'elapsed_s', y = 'class_position', color = "team_no", color_discrete_sequence=color_sequence)
     position_plot['layout']['yaxis']['autorange'] = "reversed"
     position_plot.update_layout(paper_bgcolor="black",plot_bgcolor="black")
 
@@ -224,7 +346,7 @@ def update_charts(championship, season, circuit, wec_class, session):
         cutoff_time = fia_wec_data_filtered['lap_time_ms'].min()*1.1
         box_plot_title = '110% Time Box Plots'
     fia_wec_data_with_cutoff_time = fia_wec_data_filtered[fia_wec_data_filtered['lap_time_ms'] < cutoff_time]
-    lap_time_plot = px.box(fia_wec_data_with_cutoff_time, y = 'team_no', x = 'lap_time_s', color = "team_no", color_discrete_sequence=px.colors.qualitative.Alphabet,title=box_plot_title)
+    lap_time_plot = px.box(fia_wec_data_with_cutoff_time, y = 'team_no', x = 'lap_time_s', color = "team_no", color_discrete_sequence=color_sequence,title=box_plot_title)
     lap_time_plot.update_layout(paper_bgcolor="black",plot_bgcolor="black", legend_font_color="white")
 
     return position_plot, lap_time_plot
